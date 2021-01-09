eadd0b25f45ed4ce533ef3f4246ab319fb54d1c7
nikos chalikias <nikos.chalikias@gmail.com>
2021-01-09 22:55:18 +0000

    lora TX buffer array is filled for bith string and array input datatypes

--- SimpleLoRa.cpp
+++ SimpleLoRa.cpp
@@ -35,7 +35,7 @@ nBlock_SimpleLoRa::nBlock_SimpleLoRa (
     _lora_select.setRxPayloadCrcOn(crcon);                  // Turn Off CRC
     _lora_select.setTxPower(power);                         // Set Power      
     _board.write_reg(REG_LR_PAYLOADLENGTH,payloadlength);   // Set Payload Length
-    if (cmwx1zzabz) _tcxo = 0;                               // CMWX1ZZABZ Back to Low Power
+    if (cmwx1zzabz) _tcxo = 0;                              // CMWX1ZZABZ Back to Low Power
 
     _mode = mode;
     _tx_updated = 0;
@@ -48,20 +48,23 @@ nBlock_SimpleLoRa::nBlock_SimpleLoRa (
 }
 
 void nBlock_SimpleLoRa::triggerInput(nBlocks_Message message) {   
-    if (_mode == RADIO_MODE_RX_ONLY) return;                    // Ignore inputs if we are RX only
+    if (_mode == RADIO_MODE_RX_ONLY) return;                        // Ignore inputs if we are RX only
     switch (message.dataType) {
         case OUTPUT_TYPE_INT:
         case OUTPUT_TYPE_FLOAT:
             break;
         case OUTPUT_TYPE_STRING:
-            strcpy(_tx_buffer, message.stringValue);
+            strcpy(_tx_buffer, message.stringValue);                // -------FILL THE THE _tx_buffer with the string, 
+            for (uint32_t i=0; i<256; i++){
+                _board.tx_buf[i] = _tx_buffer[i];                   // then  copy to lora _board.tx_buf
+            }
             _tx_updated = 1;
             break;
         case OUTPUT_TYPE_ARRAY:
             char * data_array = (char *)(message.pointerValue);
             for (uint32_t i=0; i<message.dataLength; i++) {
-               // _tx_buffer[i] = data_array[i];                  // --------------------FILL THE THE TX BUFFER
-               _board.tx_buf[i] = data_array[i];
+               // _tx_buffer[i] = data_array[i];                    //
+               _board.tx_buf[i] = data_array[i];                    // -----FILL THE THE lora TX BUFFER
             }
             _tx_updated = 1;
             break;          
@@ -70,12 +73,12 @@ void nBlock_SimpleLoRa::triggerInput(nBlocks_Message message) {
 
 void nBlock_SimpleLoRa::endFrame(void) {
     if (_cmwx1zzabz) _tcxo = 1;
-    if (_useleds) { _testled = !_testled; }                      // we have endFrame
+    if (_useleds) { _testled = !_testled; }                         // we have endFrame
     if (_mode != RADIO_MODE_TX_ONLY){
         if (_lora_select.service() == SERVICE_READ_FIFO) {     
-            if (_useleds) { _rxled = !_rxled; }                  // we have RX
-            output[1] = _lora_select.get_pkt_rssi();            // rssi to a the 2nd Node output           [SimpleSerial](3)
-            output[0] = (uint32_t)(&_board.rx_buf);             // the payload to the first Node output    [SimpleSerial](2)
+            if (_useleds) { _rxled = !_rxled; }                     // we have RX
+            output[1] = _lora_select.get_pkt_rssi();                // rssi to a the 2nd Node output           [SimpleSerial](3)
+            output[0] = (uint32_t)(&_board.rx_buf);                 // the payload to the first Node output    [SimpleSerial](2)
             available[0] = _payloadlength; 
             available[1] = 1;        
         }
@@ -84,10 +87,10 @@ void nBlock_SimpleLoRa::endFrame(void) {
     if (_mode != RADIO_MODE_RX_ONLY){    
         if (_tx_updated) {
             _tx_updated = 0;
-            //_board.tx_buf = _tx_buffer;                         // _radio.Transmit(_tx_buffer);
+            //_board.tx_buf = _tx_buffer;                           // _radio.Transmit(_tx_buffer);
             _lora_select.start_tx(_payloadlength);
             wait_us(2000);           
-            if (_lora_select.service() == SERVICE_TX_DONE){      // we have TX
+            if (_lora_select.service() == SERVICE_TX_DONE){         // we have TX
                 if (_useleds) { _txled = !_txled; }
             } 
         }
